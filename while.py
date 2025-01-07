825e5960c105b1053fbebcf6f91c834958621702
JavaSiro <javokhirsirojiddinov@gmail.com>
2025-01-07 15:51:14 +0900

    .

---
+++ while.py
@@ -0,0 +1,69 @@
+#b
+# nums=0
+# while nums<10:
+#     print(nums)
+#     nums+=1
+
+#c
+# nums=11
+# while nums<20:
+#     print(nums)
+#     nums+=2
+
+#d
+# nums=1000
+# while nums<1020:
+#     print(nums)
+#     nums+=2
+
+#e
+# nums=-10
+# while nums<0:
+#     print(nums)
+#     nums+=1
+
+#f
+# nums=-9
+# while nums<0:
+#     print(nums)
+#     nums+=2
+
+# g
+# nums=0
+# while nums<=8:
+#     if nums%3==0:
+#         print(nums)
+#     nums+=1
+
+#h
+# nums=10
+# while nums<=40:
+#     if nums%4==0:
+#         print(nums)
+#     nums+=1
+
+#i
+# num=0
+# while num<=4:
+#     print(num**2)
+#     num+=1
+
+#j
+# nums=2
+# while nums<=4:
+#     print(nums**3)
+#     nums+=1
+
+#k
+# nums=8
+# while nums<=12:
+#     print(f'{nums**(1/2):.1f}')
+#     nums+=1
+
+#l
+# nums=100
+# while nums<=120:
+#     print(f'{nums**(1/3):.1f}')
+#     nums+=1
+
+#m
