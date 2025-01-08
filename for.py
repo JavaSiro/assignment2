ddeba1d64c7cca0e20887dabf76d1582ae2c1daa
JavaSiro <javokhirsirojiddinov@gmail.com>
2025-01-08 19:33:04 +0900

    .

---
+++ for.py
@@ -0,0 +1,83 @@
+#b
+# for numbs in range(10):
+#     print(numbs)
+
+#c
+# for numbs in range(10,21):
+#     print(numbs)
+
+#d
+# for numbs in range(1000,2000,100):
+#     print(numbs)
+
+#e
+# list1=[1,2,3,4,5]
+# list2=[]
+# for i in range(0,len(list1)):
+#     list2.append(list1[i]**2)
+# print(list2)
+
+#f
+# fruits=['apple', 'pineapple', 'watermelon', 'banana', 'orange']
+# for i in range(0,len(fruits)):
+#     if fruits[i]=='banana':
+#         print(fruits[i])
+#         break
+#     print(fruits[i])
+
+#g
+# for i in range(1,33,3):
+#     print(i)
+
+#h
+# names=['alesha', 'bob', 'ali']
+# age=[25,30,22]
+# for a,b in zip(names,age):
+#     print(f'{a} is {b}')
+
+#i
+# for i in range(1,21):
+#     if i%2==0:
+#         print(i)
+
+#j
+# nums=[7,2,9,15,4,8,5]
+# print(min(nums), max(nums))
+
+#k,l
+# nums=[1,2,3,4,5,6,7,8,9,10]
+# for i in range(1,11):
+#     if i%2==0:
+#         print(i) 
+#it has to be used seperately 
+    # if not i%2==0:
+    #     print(i)
+
+#m
+# for i in range(-20,-9):
+#     print(i)
+
+#n
+# for i in range (-19,-10, 2):
+#     print(i)
+
+#o
+# for i in range(-10,0, 2):
+#     print(i)
+
+#p
+# for i in range(0,8):
+#     if i%3==0:
+#         print(i)
+
+#q
+# for i in range(2,11):
+#     print(i**2)
+
+#r
+# for i in range(2,11):
+#     print(i**3)
+
+#s
+# for i in range(9,16):
+#     print(f'{i**0.5:.2f}')
